01df75af777b45221b220a3eaa7e609f53aea9a9
Raj Parihar <mrrajparihar281@gmail.com>
2025-08-28 18:40:09 +0530

    page.js updated

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -5,9 +5,9 @@ export default function Hero() {
   return (
     <section className={styles.hero}>
       <div className={styles.heroContent}>
-        <h1 className={styles.title}>Your Name</h1>
+        <h1 className={styles.title}>Raj Parihar</h1>
         <p className={styles.subtitle}>
-          Im a Web Developer specializing in creating modern web applications.
+          Im a Flutter Developer specializing in creating modern Mobile applications.
         </p>
         <a href="#projects" className={styles.ctaButton}>
           View My Work
