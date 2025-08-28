824e6445d060ee09e81486a29eb1a0a29cb184f7
Raj Parihar <mrrajparihar281@gmail.com>
2025-08-28 18:37:44 +0530

    Hero added to page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
+import Hero from "./components/Hero";
 export default function Home() {
   return (
     <div>
       <Header /> {/* Use the Header component */}
-      <main>
-        <h2>Welcome!</h2>
-        <p>This is the main section of my portfolio.</p>
-      </main>
+      <Hero /> {/* 2. Use the Hero component */}
       <Footer /> {/* Use the Footer component */}
     </div>
   );
