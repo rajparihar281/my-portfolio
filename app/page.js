e969a1eefefa68f59365e8ba2809b392dedec783
Raj Parihar <mrrajparihar281@gmail.com>
2025-08-28 18:14:58 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main
+      style={{
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        marginTop: "50px",
+      }}
+    >
+      <h1>Welcome to My Portfolio</h1>
+      <p>This site is under construction!</p>
+    </main>
+  );
+}
